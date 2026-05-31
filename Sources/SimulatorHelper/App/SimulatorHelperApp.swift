5d48c2f0b507af58b4b84738ba567aa6bd05eebf
tom-pudding <yuatomo@gmail.com>
2026-06-01 00:51:44 +0900

    Activate app window on launch

--- Sources/SimulatorHelper/App/SimulatorHelperApp.swift
+++ Sources/SimulatorHelper/App/SimulatorHelperApp.swift
@@ -1,11 +1,26 @@
+import AppKit
 import SwiftUI
 
 @main
 struct SimulatorHelperApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .frame(minWidth: 960, minHeight: 620)
+        }
+    }
+}
+
+private final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+
+        DispatchQueue.main.async {
+            NSRunningApplication.current.activate(options: [.activateIgnoringOtherApps])
+            NSApp.activate(ignoringOtherApps: true)
+            NSApp.windows.first?.makeKeyAndOrderFront(nil)
         }
     }
 }
